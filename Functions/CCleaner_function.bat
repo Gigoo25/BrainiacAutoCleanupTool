82bf9705586e4bb0823ebe8acadcf17677759b4a
Gigoo25 <rstocchi@buckeyehq.com>
2019-06-11 10:24:11 -0400

    -Start CCleaner registry cleanup in maximized window

--- Functions/CCleaner_function.bat
+++ Functions/CCleaner_function.bat
@@ -6,14 +6,14 @@ if exist "%systemdrive%\Program Files\CCleaner\CCleaner.exe" (
 	REM Display message that CCleaner is installed & run
 	%Output%\Functions\Menu\MessageBox "Starting CCleaner temp files cleanup.\n\nThis may take a while." "[ALERT] Brainiacs Cleanup Tool v%TOOL_VERSION%" /B:O /I:X /O:N /T:5 >nul
 	REM Run appropriate version of ccleaner temp file scan
-	if %OS%==32BIT "%systemdrive%\Program Files\CCleaner\CCleaner.exe" /AUTO
-	if %OS%==64BIT "%systemdrive%\Program Files\CCleaner\CCleaner64.exe" /AUTO
+	if %OS%==32BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" "%systemdrive%\Program Files\CCleaner\CCleaner.exe" /AUTO
+	if %OS%==64BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" "%systemdrive%\Program Files\CCleaner\CCleaner64.exe" /AUTO
 	REM Add notes that finished
 	echo -Ran CCleaner temp file cleanup >> %Output%\Notes\Comments.txt
 	REM Display message that CCleaner is installed & run
 	%Output%\Functions\Menu\MessageBox "Done\u0021\n\nOpening CCleaner registry cleanup." "[ALERT] Brainiacs Cleanup Tool v%TOOL_VERSION%" /B:O /I:X /O:N /T:5 >nul
-	if %OS%==32BIT "%systemdrive%\Program Files\CCleaner\CCleaner.exe" /REGISTRY
-	if %OS%==64BIT "%systemdrive%\Program Files\CCleaner\CCleaner64.exe" /REGISTRY
+	if %OS%==32BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" /MAX "%systemdrive%\Program Files\CCleaner\CCleaner.exe" /REGISTRY
+	if %OS%==64BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" /MAX "%systemdrive%\Program Files\CCleaner\CCleaner64.exe" /REGISTRY
 	REM Add notes that finished
 	echo -Ran CCleaner registry cleanup >> %Output%\Notes\Comments.txt
 	GOTO :EOF
@@ -22,15 +22,15 @@ if exist "%systemdrive%\Program Files (x86)\CCleaner\CCleaner.exe" (
 	REM Display message that CCleaner is installed & run
 	%Output%\Functions\Menu\MessageBox "Starting CCleaner temp files cleanup.\n\nThis may take a while." "[ALERT] Brainiacs Cleanup Tool v%TOOL_VERSION%" /B:O /I:X /O:N /T:5 >nul
 	REM Run appropriate version of ccleaner temp file scan
-	if %OS%==32BIT "%systemdrive%\Program Files (x86)\CCleaner\CCleaner.exe" /AUTO
-	if %OS%==64BIT "%systemdrive%\Program Files (x86)\CCleaner\CCleaner64.exe" /AUTO
+	if %OS%==32BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" "%systemdrive%\Program Files (x86)\CCleaner\CCleaner.exe" /AUTO
+	if %OS%==64BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" "%systemdrive%\Program Files (x86)\CCleaner\CCleaner64.exe" /AUTO
 	REM Add notes that finished
 	echo -Ran CCleaner temp file cleanup >> %Output%\Notes\Comments.txt
 	REM Display message that CCleaner is installed & run
 	%Output%\Functions\Menu\MessageBox "Done\u0021\n\nOpening CCleaner registry cleanup." "[ALERT] Brainiacs Cleanup Tool v%TOOL_VERSION%" /B:O /I:X /O:N /T:5 >nul
 	REM Run appropriate version of ccleaner temp file scan
-	if %OS%==32BIT "%systemdrive%\Program Files (x86)\CCleaner\CCleaner.exe" /REGISTRY
-	if %OS%==64BIT "%systemdrive%\Program Files (x86)\CCleaner\CCleaner64.exe" /REGISTRY
+	if %OS%==32BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" /MAX "%systemdrive%\Program Files (x86)\CCleaner\CCleaner.exe" /REGISTRY
+	if %OS%==64BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" /MAX "%systemdrive%\Program Files (x86)\CCleaner\CCleaner64.exe" /REGISTRY
 	REM Add notes that finished
 	echo -Ran CCleaner registry cleanup >> %Output%\Notes\Comments.txt
 	GOTO :EOF
@@ -39,15 +39,15 @@ if exist "%Output%\Tools\CCleaner\CCleane*" (
 	REM Display message that CCleaner is not installed & run portable version
 	%Output%\Functions\Menu\MessageBox "Starting portable CCleaner temp files cleanup.\n\nThis may take a while." "[ALERT] Brainiacs Cleanup Tool v%TOOL_VERSION%" /B:O /I:X /O:N /T:5 >nul
 	REM Run appropriate version of portable ccleaner temp file scan
-	if %OS%==32BIT "%Output%\Tools\CCleaner\CCleaner.exe" /AUTO
-	if %OS%==64BIT "%Output%\Tools\CCleaner\CCleaner64.exe" /AUTO
+	if %OS%==32BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" "%Output%\Tools\CCleaner\CCleaner.exe" /AUTO
+	if %OS%==64BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" "%Output%\Tools\CCleaner\CCleaner64.exe" /AUTO
 	REM Add notes that finished
 	echo -Ran CCleaner temp file cleanup >> %Output%\Notes\Comments.txt
 	REM Display message that CCleaner is not installed & run portable version
 	%Output%\Functions\Menu\MessageBox "Done\u0021\n\nOpening portable CCleaner registry cleanup." "[ALERT] Brainiacs Cleanup Tool v%TOOL_VERSION%" /B:O /I:X /O:N /T:5 >nul
 	REM Run appropriate version of ccleaner temp file scan
-	if %OS%==32BIT "%Output%\Tools\CCleaner\CCleaner.exe" /REGISTRY
-	if %OS%==64BIT "%Output%\Tools\CCleaner\CCleaner64.exe" /REGISTRY
+	if %OS%==32BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" /MAX "%Output%\Tools\CCleaner\CCleaner.exe" /REGISTRY
+	if %OS%==64BIT start /WAIT "[CCleaner] Brainiacs Cleanup Tool v%TOOL_VERSION%" /MAX "%Output%\Tools\CCleaner\CCleaner64.exe" /REGISTRY
 	REM Add notes that finished
 	echo -Ran CCleaner registry cleanup >> %Output%\Notes\Comments.txt
 	GOTO :EOF
