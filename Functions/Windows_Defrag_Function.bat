1813f8a80e7aeaa058394086b39383cfd52af0ea
Gigoo25 <rstocchi@buckeyehq.com>
2019-06-12 14:38:49 -0400

    -Added ability for boot defrag to only re-attempt 2 times before stopping.

--- Functions/Windows_Defrag_Function.bat
+++ Functions/Windows_Defrag_Function.bat
@@ -1,21 +1,26 @@
 @echo off
 
 REM Variables
-set Defrag_On_Boot=unidentified
+set Defrag_On_Boot=Yes
+set Defrag_On_Boot_Counter=0
 
 REM Enable delayed expansion
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM Determine how the function was ran
+REM Determine if it was ran from boot
 if exist "%SystemDrive%\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Windows_Defrag_Function.bat" (
   set Defrag_On_Boot=Yes
+)
+
+REM Run function according to how it was ran
+if "%Defrag_On_Boot%"=="Yes" (
   goto Start_Defrag_On_Boot
 )
 if "%Defrag_External%"=="Yes" (
-    goto Start_External_Defrag
+  goto Start_External_Defrag
 )
 if "%Defrag_External%"=="No" (
-    goto Start_Internal_Defrag
+  goto Start_Internal_Defrag
 )
 
 REM Start defrag externally
@@ -55,36 +60,81 @@ if /i !ERRORLEVEL!==0 (
 
 REM Start defrag on boot
 :Start_Defrag_On_Boot
-start /WAIT "[Defrag] Brainiacs Cleanup Tool v%TOOL_VERSION%" %SystemRoot%\system32\defrag.exe "%SystemDrive%" /O /V /H /U
-if /i !ERRORLEVEL!==0 (
-  echo.
-  echo  ^! ALERT
-  echo ===========================
-  echo.
-  echo    Defrag complete!
-  echo.
-  echo    Press any key to quit.
-  echo.
-  echo ===========================
-  REM Self delete if ran after boot/any other circumstance
-  (goto) 2>nul & del "%~f0"
-  GOTO :EOF
-) else (
-  REM Loop to Defrag_On_Boot_Loop to try again
-  color 0c
+REM Set title
+title [Defrag] Brainiacs Cleanup Tool v%TOOL_VERSION%
+REM Check for more than 3 tries then quit
+if %Defrag_On_Boot_Counter%==3 (
   CLS
+  REM Set color
+  color 0c
   echo.
   echo  ^! ERROR
-  echo =============================================================================
+  echo ==============================================================================
   echo.
   echo    Windows defrag failed!
   echo.
   echo    The Brainiacs Cleanup Tool v%TOOL_VERSION% will try again in 15 seconds.
   echo.
-  echo =============================================================================
+  echo ==============================================================================
   TIMEOUT 15
+  REM Set color
   color 07
+  REM Self delete if defrag failed to run after 3 consecutive tries
+  (goto) 2>nul & del "%~f0"
+  GOTO :EOF
+)
+REM Start defrag
+REM start /WAIT "[Defrag] Brainiacs Cleanup Tool v%TOOL_VERSION%" %SystemRoot%\system32\defrag.exe "%SystemDrive%" /O /V /H /U
+set ERRORLEVEL=1
+if /i !ERRORLEVEL!==0 (
+  CLS
+  echo.
+  echo  ^! ALERT
+  echo ============================
+  echo.
+  echo    Defrag complete!
+  echo.
+  echo    Press any key to quit.
+  echo.
+  echo ============================
+  REM Self delete if ran after boot/any other circumstance
+  (goto) 2>nul & del "%~f0"
+  GOTO :EOF
+) else (
+  REM Loop to Defrag_On_Boot_Loop to try again
+  CLS
+  REM Set color
+  color 0c
+  echo.
+  echo  ^! ERROR
+  echo ==============================================================================
+  echo.
+  echo    Windows defrag failed!
+  echo.
+  REM Display correct amount of times
+  if %Defrag_On_Boot_Counter%==0 (
+    echo    Will try to defrag 3 more times before quitting.
+  )
+  if %Defrag_On_Boot_Counter%==1 (
+  echo    Will try to defrag 2 more times before quitting.
+  )
+  if %Defrag_On_Boot_Counter%==2 (
+  echo    Will try to defrag 1 more time before quitting.
+  )
+  echo.
+  echo    The Brainiacs Cleanup Tool v%TOOL_VERSION% will try again in 15 seconds.
+  echo.
+  echo ==============================================================================
+  TIMEOUT 15
+  REM Set color
+  color 07
+  REM Set variable
   set Defrag_On_Boot=No
+  REM Set counter for amount of tries
+  if %Defrag_On_Boot_Counter%==0 set Defrag_On_Boot_Counter=1
+  if %Defrag_On_Boot_Counter%==1 set Defrag_On_Boot_Counter=2
+  if %Defrag_On_Boot_Counter%==2 set Defrag_On_Boot_Counter=3
+  REM Re-try to run defrag
   goto :Start_Defrag_On_Boot
 )
 
